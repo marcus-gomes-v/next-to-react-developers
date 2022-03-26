a7dd3fa082de499f9ac5a7c44feb76fc75e1c3b5
M.V. Bego <viniciusgomes@users.noreply.github.com>
2022-03-26 19:41:52 +0000

    Creating pt-03-navigate-between-pages
        This is simple navigation page using the **link component** from next, that is amazing cause help you to use links on client sife if you have javascript availabe but also link pages if you js is disabled.
    
        Really amazing feature from Next.JS team.

---
+++ pt-03-navigate-between-pages/pages/index.js
@@ -0,0 +1,131 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          <a href="https://nextjs.org">Next.js</a>
+          {' '}by{' '}
+          <a href="https://decriptando.blogspot.com">Decriptando</a>
+        </h1>
+
+        <p className="description">
+          Bora codar simples, bora decriptar o conhecimento. 
+        </p>
+
+        <hr/>
+
+        <p>
+          Navigate to{' '}
+          <Link href="/posts/first-post">
+            <a>first page!</a>
+          </Link>
+        </p>
+      </main>
+
+      
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: dodgerblue;
+          text-decoration: none;
+        }
+
+        .title a:last-child {
+          color: coral;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        p a {
+          color: dodgerblue;
+          text-decoration: none;
+        }
+
+    `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
